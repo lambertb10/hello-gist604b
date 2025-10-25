465ca1038325ed9197aceb7b926e796c118cfa93
Brendan Lambert <Lambertbk10@gmail.com>
2025-10-25 12:03:43 -0400

    Update hello.py

--- scripts/hello.py
+++ scripts/hello.py
@@ -16,7 +16,7 @@ def main():
     print("University of Arizona coordinates: 32.2319° N, 110.9501° W")
     
     # Add your own message here!
-    print("University of Arizona coordinates(MGRS): 12S WA 04701 66141/nMGRS is the format I am more comfortable with")
+    print("My Own Message")
 
 if __name__ == "__main__":
     main()
